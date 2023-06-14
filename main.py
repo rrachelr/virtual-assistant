1eddf5e0fb7a75485722ab64e65a18720ede31bf
rrachelr <rachrobins1@gmail.com>
2023-06-14 14:56:36 -0400

    Completed welcome function

---
+++ main.py
@@ -0,0 +1,14 @@
+'''
+Author: Rachel Robins
+Updated: 6/14/23
+Description: This program runs the virtual assistant "Aida" who will
+execute tasks according to the user's input.
+'''
+USER_NAME = 'Friend'
+
+def welcome():
+    print(f'Welcome back, {USER_NAME}!')
+
+if __name__ == "__main__":
+    welcome()
+    
